6b33f001b15bdb6402c94fb995914b4bf6a58cd6
codebro <samsondamilola.99@gmail.com>
2025-03-08 00:59:20 +0100

    add new whitelist

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 
-const allowedDomains = ['http://localhost:5173', 'http://localhost:3000'];
+const allowedDomains = ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5000'];
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin || allowedDomains.includes(origin)) {
