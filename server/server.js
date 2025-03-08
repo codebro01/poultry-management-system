a47449b0d78274d8074ba9a5f514e0ad1ad503b9
codebro <samsondamilola.99@gmail.com>
2025-03-08 01:29:04 +0100

    added vercel url to whitelist

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 
-const allowedDomains = ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5000'];
+const allowedDomains = ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5000','https://poultry-management-system-ten.vercel.app/'];
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin || allowedDomains.includes(origin)) {
